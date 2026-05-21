e01b2e0038a1fe6fed404b87aa4f42db28665334
m1lost <m1lost@live.com>
2026-05-21 12:44:58 +0700

    update has router

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,20 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import 'react-toastify/dist/ReactToastify.css';
-
-// import './index.css';
 
 import App from './App.jsx';
 import store from './app/store';
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </Provider>
 );
+
