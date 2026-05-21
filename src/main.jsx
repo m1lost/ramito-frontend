9e94af62f923dede549be352cf1d7fd6de79aee4
m1lost <m1lost@live.com>
2026-05-21 12:49:08 +0700

    update browrouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import 'react-toastify/dist/ReactToastify.css';
+
+// import './index.css';
 
 import App from './App.jsx';
 import store from './app/store';
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <HashRouter>
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </Provider>
 );
-
